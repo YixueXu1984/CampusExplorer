316f377158c5ad4fbc646419b1cf6d279726734e
TI130 <tyronemonitor@gmail.com>
2018-09-27 13:11:52 -0700

    ListDatasets in progress

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -26,6 +26,49 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
-        return Promise.reject("Not implemented.");
+        // 1, Check data structure: text
+        // 2. Read datasets' id
+        // 3. Read datasets' type
+        // 4. Read datasets' number of rows
+        // 5. Return fullfilled promises with all the data
+        let datasets: InsightDataset[];
+        let dataset: InsightDataset;
+        const path = "./src/datasets";
+        const fs   = require("fs");
+        let files = fs.readdirSync(path);
+
+        for (let file in files) {
+            // create new datasets and add them to datasets
+            // todo
+            let text = fs.readFileSync(file, "utf8");
+            dataset = this.createDataset(this.getId(text), this.getType(text), this.getNumRows(text));
+            datasets.push(dataset);
+        }
+        return Promise.resolve(datasets);
+    }
+
+    public createDataset(name: string, type: InsightDatasetKind, num: number): InsightDataset {
+        let dataset: InsightDataset;
+        dataset.id      = name;
+        dataset.kind    = type;
+        dataset.numRows = num;
+        return dataset;
+    }
+
+    public getId(text: string): string {
+        // todo
+        let arr = text.split(";");
+        arr[1].trim();
+        return text.substring(arr[1].indexOf("id"));
+    }
+
+    public getType(text: string): InsightDatasetKind {
+        // todo
+        return null;
+    }
+
+    public getNumRows(text: string): number {
+        // todo
+        return -1;
     }
 }
