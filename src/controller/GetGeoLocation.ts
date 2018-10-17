85203101917386d100f2311ddba445cce5516f7d
yixue <v7e0b@ugrad.cs.ubc.ca>
2018-10-17 00:26:34 -0700

    update getGeoLocation stub

---
+++ src/controller/GetGeoLocation.ts
@@ -0,0 +1,18 @@
+import {JSZipObject} from "jszip";
+import {IRoom} from "../model/Room";
+import * as JSZip from "jszip";
+import {InsightDatasetKind} from "./IInsightFacade";
+import {IDataSet} from "../model/DataSet";
+
+export default class GetGeoLocation  {
+
+    constructor()  {
+        // stub
+    }
+
+    public getGeoLocation(Location: string): Promise<[number, number]>  {
+        // stub
+        return null;
+    }
+
+}
